089d0d8a5cc4e083af5df3cf8deac26082abda10
monfilsronald35-creator <monfilsronald35@gmail.com>
2026-05-16 18:59:03 -0400

    fix root lock file

--- services/fraud-service/src/middleware/notFoundHandler.js
+++
@@ -1,9 +0,0 @@
-const notFoundHandler = (req, res) => {
-  res.status(404).json({
-    error: 'Route not found',
-    path: req.originalUrl,
-    method: req.method
-  });
-};
-
-module.exports = notFoundHandler;
