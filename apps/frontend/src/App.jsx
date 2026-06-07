ceec18d046c1546b039a411947da8392b122ff41
monfilsronald35-creator <monfilsronald35@gmail.com>
2026-06-07 10:04:25 -0400

    Update App.jsx

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-// 🔥 Enpòte AuthProvider
 import { AuthProvider } from "./context/AuthContext"; 
-// Enpòte AppRoutes nou te kreye anvan an
 import AppRoutes from "./routes/AppRoutes";
+import "./styles/master.css"; 
 
 function App() {
   return (
     <AuthProvider>
-      {/* Tout navigasyon an ak sekirite paj yo (PrivateRoute) 
-         kounye a jere nan AppRoutes.jsx 
-      */}
       <AppRoutes />
     </AuthProvider>
   );
