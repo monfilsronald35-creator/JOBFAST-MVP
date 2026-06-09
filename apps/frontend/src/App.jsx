5a150cd95cddd74f76252fd5e69263626840bbbf
monfilsronald35-creator <monfilsronald35@gmail.com>
2026-06-08 20:48:00 -0400

    fix: update master.css to global.css in App.jsx

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { AuthProvider } from "./context/AuthContext"; 
+import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
-import "./styles/master.css"; 
+import "./styles/global.css";
 
 function App() {
   return (
@@ -11,4 +11,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;        
